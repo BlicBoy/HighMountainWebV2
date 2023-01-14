98684f0388c2e9afe23392d8fa1e061b5991475a
Patrícia <patriciaoliveira20022@gmail.com>
2023-01-14 18:20:09 +0000

    Percursos atualização

--- src/listPercursos.js
+++ src/listPercursos.js
@@ -1,4 +1,4 @@
-import { viewImage } from "./index.js"
+//import { viewImage } from "./index.js"
 
 
 const addPercursoToList = async (doc) => {
@@ -8,14 +8,14 @@ const addPercursoToList = async (doc) => {
 
 
     console.log(percurso.photoPercurso)
-    await viewImage(percurso.photoPercurso, "percurso-imagem")
-    html += "<img class ='col-md-4 img-fluid' id = 'percurso-imagem' src=''>";
-    html += "<p> Nome = '" + percurso.Nome + "</p> ";
-    html += "<p> Data de Início = '" + percurso.DataInicio + "</p>";
-    html += "<p> Hora de Início = '" + percurso.HoraInicio + "</p>";
-    html += "<p> Nome do Instrutor = '" + percurso.NomeCriador + "</p>";
-
+    //await viewImage(percurso.photoPercurso, "percurso-imagem")
+    //html += "<img class ='col-md-4 img-fluid' id = 'percurso-imagem' src=''>";
     html += "<hr>";
+    html += "<span class=''>" + percurso.Nome + "</span>";
+    html += "<button class='btn btn-primary float-right'> Saber Mais </button>"
+    //html += "<p> Data de Início = '" + percurso.DataInicio + "</p>";
+    //html += "<p> Hora de Início = '" + percurso.HoraInicio + "</p>";
+    //html += "<p> Nome do Instrutor = '" + percurso.NomeCriador + "</p>";
 
     return html;
 }
